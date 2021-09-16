55c058275759c4a0fe84b20e6ad1950d4e9f9b18
kevinramirezz1 <ke.ramirez30@gmail.com>
2021-09-16 00:46:04 -0600

    comienzo de proyecto

---
+++ permutacin-combinacion.cpp
@@ -0,0 +1,15 @@
+//Project created by Kevin Ramirezz
+//ig @kecin_ramirezz1
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iomanip>
+
+
+using namespace std;
+
+
+//© Todos los derechos reservados
